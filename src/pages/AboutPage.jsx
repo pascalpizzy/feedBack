c68e9a6fe7cd427c2e13e8e9d3081a6db788ac6d
pascalpizzy <pascalikechukwu@gmail.com>
2022-11-28 11:59:15 -0500

    My first react App

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,16 @@
+import Card from "../components/shared/Card"
+import {Link} from 'react-router-dom'
+
+
+function AboutPage() {
+  return  <Card>
+    <div>This is the About us page feel free to know us btter </div>
+
+        <Link to ='/'>
+            return home
+        </Link>
+    </Card>
+    
+}
+
+export default AboutPage
