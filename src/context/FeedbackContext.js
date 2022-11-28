d34a4953d9086dca798baa9c17746cd0ac932206
pascalpizzy <pascalikechukwu@gmail.com>
2022-11-28 17:59:12 -0500

    added JSON database

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,37 +1,35 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import {v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
 
-    const[feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This is Feedback item 1', 
-            rating: 10
-        },
-        {
-            id: 2,
-            text: 'This is Feedback item 2', 
-            rating: 7
-        },
-        {
-            id: 3,
-            text: 'This is Feedback item 3', 
-            rating: 9
-        },
-    ])
+    const[feedback, setFeedback] = useState([])
 
     const[feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
 
+    useEffect(() => {
+       fetchFeedback()
+    }, [])
 
+    //Fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+        const data = await response.json()
+
+        setFeedback(data)
+    }
+
+    
     //delete feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if(window.confirm('Are you sure you want to Delete?')){
+
+            await fetch(`http://localhost:5000/feedback/${id}`, {method: 'DELETE'})
     
           setFeedback(feedback.filter((item) => item.id !== id))
         }
@@ -54,9 +52,16 @@ export const FeedbackProvider = ({children}) => {
 
 
       //to add feedback
-      const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback,...feedback])
+      const addFeedback = async (newFeedback) => {
+        const response = await fetch('http://localhost:5000/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newFeedback),
+        })
+        const data = await response.json()
+        setFeedback([data,...feedback])
       }
 
     return <FeedbackContext.Provider value={{
